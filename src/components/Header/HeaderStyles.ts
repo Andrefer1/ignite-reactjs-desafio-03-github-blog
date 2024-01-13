08c811674775eeb5bdc1fd4e324f7a5df768611a
André F. Bispo <andrefer483@gmail.com>
2024-01-13 02:12:42 -0300

    Component Header was added

---
+++ src/components/Header/HeaderStyles.ts
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-between;
+    background: #0b1b2b;
+`;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,14 @@
+import GithubBlogLogo from '../../assets/github-blog-logo.svg'
+import SideEffectLeft from '../../assets/side-effect-left.svg'
+import SideEffectRight from '../../assets/side-effect-right.svg'
+import { HeaderContainer } from './HeaderStyles'
+
 export function Header() {
     return (
-        <div>Header</div>
+        <HeaderContainer>
+            <img src={SideEffectLeft} alt='Efeito lateral esquerdo do cabeçalho' />
+            <img src={GithubBlogLogo} alt='Logo do Github Blog' />
+            <img src={SideEffectRight} alt='Efeito lateral direito do cabeçalho' />
+        </HeaderContainer>
     )
 }
