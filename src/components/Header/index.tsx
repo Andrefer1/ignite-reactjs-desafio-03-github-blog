1656e37d338fba13cd1ca6da31bafeb2da3d6ad9
André F. Bispo <andrefer483@gmail.com>
2024-01-13 17:50:47 -0300

    The component Header was adjusted

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,11 @@
-import GithubBlogLogo from '../../assets/github-blog-logo.svg'
-import SideEffectLeft from '../../assets/side-effect-left.svg'
-import SideEffectRight from '../../assets/side-effect-right.svg'
 import { HeaderContainer } from './styles'
+
+import GithubBlogLogo from '../../assets/github-blog-logo.svg'
 
 export function Header() {
     return (
         <HeaderContainer>
-            <img src={SideEffectLeft} alt='Efeito lateral esquerdo do cabeçalho' />
             <img src={GithubBlogLogo} alt='Logo do Github Blog' />
-            <img src={SideEffectRight} alt='Efeito lateral direito do cabeçalho' />
         </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,14 @@
 import { styled } from 'styled-components';
 
+import HeaderBackground from '../../assets/header-background.svg';
+
 export const HeaderContainer = styled.header`
     display: flex;
-    justify-content: space-between;
-    background: ${(props) => props.theme.blue850};
+    justify-content: center;
+    height: 18.5rem;
+    background: url(${HeaderBackground}) no-repeat center/cover;
+
+    img {
+        object-fit: none;
+    }
 `;
