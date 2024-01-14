b74fb93b977e5409a02b1f360f4ce8a379350807
André F. Bispo <andrefer483@gmail.com>
2024-01-14 03:09:35 -0300

    Profile component was finished

--- src/pages/Feed/components/Profile/index.tsx
+++ src/pages/Feed/components/Profile/index.tsx
@@ -1,5 +1,82 @@
-import { ProfileContainer } from './styles';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+    faArrowUpRightFromSquare,
+    faBuilding,
+    faUserGroup,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { api } from '../../../../libs/axios';
+import { ContentContainer, ProfileContainer } from './styles';
+
+interface User {
+    avatar_url: string;
+    bio: string;
+    company: string;
+    followers: number;
+    login: string;
+    name: string;
+    url: string;
+    public_repos: number;
+    repos_url: string;
+}
 
 export function Profile() {
-    return <ProfileContainer>André Fernandes Bispo</ProfileContainer>;
+    const [user, setUser] = useState<User>({} as User);
+
+    async function fetchUserDataFromGitHub() {
+        const response = await api.get(`/users/andrefer1`);
+        const data = response.data;
+        setUser(data);
+    }
+
+    useEffect(() => {
+        fetchUserDataFromGitHub();
+    }, []);
+
+    return (
+        <ProfileContainer>
+            <ContentContainer>
+                <aside>
+                    <img src={user.avatar_url} alt="User profile photo" />
+                </aside>
+
+                <main>
+                    <header>
+                        <h1>{user.name}</h1>
+
+                        <Link to={'/'}>
+                            GITHUB
+                            <FontAwesomeIcon
+                                size="xs"
+                                icon={faArrowUpRightFromSquare}
+                            />
+                        </Link>
+                    </header>
+
+                    <p>{user.bio}</p>
+
+                    <footer>
+                        <div>
+                            <FontAwesomeIcon icon={faGithub} />
+                            <div className="text-small">{user.login}</div>
+                        </div>
+                        {user.company && (
+                            <div>
+                                <FontAwesomeIcon icon={faBuilding} />
+                                <div className="text-small">{user.company}</div>
+                            </div>
+                        )}
+                        <div>
+                            <FontAwesomeIcon icon={faUserGroup} />
+                            <div className="text-small">
+                                {user.followers} seguidores
+                            </div>
+                        </div>
+                    </footer>
+                </main>
+            </ContentContainer>
+        </ProfileContainer>
+    );
 }
--- src/pages/Feed/components/Profile/styles.ts
+++ src/pages/Feed/components/Profile/styles.ts
@@ -4,6 +4,61 @@ export const ProfileContainer = styled.div`
     width: 54rem;
     height: 13.25rem;
     border-radius: 10px;
-    background: ${(props) => props.theme.profile};
+    background: ${(props) => props.theme.colors.base.background};
     box-shadow: 0px 2px 28px 0px rgba(0, 0, 0, 0.2);
+    padding: 2rem 2.5rem;
+    margin-top: -90px;
+`;
+
+export const ContentContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+
+    aside {
+        margin-right: 2rem;
+
+        img {
+            height: 9.25rem;
+            width: 9.25rem;
+            border-radius: 8px;
+        }
+    }
+
+    main {
+        display: flex;
+        flex-direction: column;
+
+        header {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 0.5rem;
+
+            h1 {
+                margin-top: 0.5rem;
+            }
+
+            a {
+                svg {
+                    margin-left: 0.5rem;
+                    color: ${(props) => props.theme.colors.brand.blue};
+                }
+            }
+        }
+
+        p {
+            flex-grow: 1;
+        }
+
+        footer {
+            display: flex;
+            gap: 1.5rem;
+
+            div {
+                display: flex;
+                align-items: center;
+                gap: 0.5rem;
+            }
+        }
+    }
 `;
